22d37331983546c66d9457842e87563b2d810cef
Yurii Tomashevskyi <niteris92@gmail.com>
2024-12-02 18:54:43 +0200

    Added migration file to ns-contract-service

--- ns-contract-service/src/ormconfig.test.ts
+++ ns-contract-service/src/ormconfig.test.ts
@@ -1,10 +1,11 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { config as dotenvConfig } from "dotenv";
-import { Contract } from "./contracts/contract.entity";
+import 'reflect-metadata';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config as dotenvConfig } from 'dotenv';
+import { Contract } from './contracts/contract.entity';
 
-dotenvConfig({ path: ".env.test" });
+dotenvConfig({ path: '.env.test' });
 
-export const testDatabaseConfig: TypeOrmModuleOptions = {
+export const testDatabaseConfig: DataSourceOptions = {
   type: "postgres",
   host: process.env.DATABASE_HOST_TEST!,
   port: parseInt(process.env.DATABASE_PORT_TEST!, 10),
@@ -12,7 +13,8 @@ export const testDatabaseConfig: TypeOrmModuleOptions = {
   password: process.env.DATABASE_PASSWORD_TEST!,
   database: process.env.DATABASE_NAME_TEST!,
   entities: [Contract],
-  dropSchema: true,
-  synchronize: true,
-  autoLoadEntities: true,
+  migrations: ['migrations/*.ts'],
+  synchronize: false,
 };
+
+export const AppDataSource = new DataSource(testDatabaseConfig);
